f3b46a811a7b45ae4be29bebbb8c5d722f90fef8
Daniil Zaytsev <zdanil34@gmail.com>
2023-09-14 08:21:01 +0700

    add tests

--- python/cli/src/cli.py
+++ python/cli/src/cli.py
@@ -1,23 +1,24 @@
-from pathlib import Path
-import sys
 import argparse
-from typing import NamedTuple
+import sys
+from dataclasses import dataclass
+from pathlib import Path
 
 
-class Arguments(NamedTuple):
+@dataclass(frozen=True)
+class Arguments:
     file_in: Path | None = None
     file_out: Path | None = None
 
-
-def parse_cli() -> Arguments:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", dest="file_in", type=Path)
-    parser.add_argument("-o", dest="file_out", type=Path)
-    return Arguments(**vars(parser.parse_args()))
+    @classmethod
+    def from_arguments(cls) -> "Arguments":
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-i", dest="file_in", type=Path)
+        parser.add_argument("-o", dest="file_out", type=Path)
+        return cls(**vars(parser.parse_args()))
 
 
 def main():
-    args = parse_cli()
+    args = Arguments.from_arguments()
 
     input_stream = args.file_in.open(encoding="utf-8") if args.file_in else sys.stdin
     output_stream = args.file_out.open("w", encoding="utf-8") if args.file_out else sys.stdout
---
+++ python/cli/tests/test_cli.py
@@ -0,0 +1,12 @@
+import subprocess
+
+
+def test_base():
+    in_ = "hello world"
+    out_ = f"processed: {in_}"
+    pipe = subprocess.Popen(["my_py_cli"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+    out, err = pipe.communicate(in_)
+
+    assert pipe.returncode == 0
+    assert out_ in out
+
