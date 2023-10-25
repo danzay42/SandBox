4934d526aaa5f021b93bc775d4c222b31d9fb926
Daniil Zaytsev <zdanil34@gmail.com>
2023-10-25 23:48:23 +0700

    add pipe info

--- linux/bash/pipe.sh
+++ linux/bash/pipe.sh
@@ -9,16 +9,17 @@ echo    "command1 || " || echo "command2" # executed if command1 is faild
 echo $(sleep 0.1; echo "command1") & echo -n "command2 &  " # command1 is background shell task
 sleep 0.2
 
-# piping streams: |, <, <<, >, >>, tee, xargs
-
-# |     - redirect from stdout to stdin
-# tee   - duplicate stdout to file
-# xargs - redirect from stdin to command arguments 
-# >     - create file and add to file
-# >>    - add to file
-# <     - ?
-# <<    - ?
-# <<<   - ?
+# piping streams: |, <, <<, <<<, < <, >, >>, tee, xargs
+# https://askubuntu.com/questions/678915/whats-the-difference-between-and-in-bash
+# |      - redirect from stdout to stdin
+# tee    - duplicate stdout to file
+# xargs  - redirect from stdin to command arguments 
+# >      - create empty file and write stdout to file
+# >>     - write stdout to file
+# <      - read from file to stdin
+# << EOF - `here-document` read text block to stdin
+# <<<    - `here-string` read string to stdin
+# < <    - 'Process Substitution' 
 
 # 0 /dev/stdin
 # 1 /dev/stdout
